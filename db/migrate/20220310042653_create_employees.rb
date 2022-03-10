9cacfd29fd711c3988447c9d7a4d0b79b9be3e1f
Victor <victorrivera714@yahoo.com>
2022-03-10 15:22:08 -0800

    tables

---
+++ db/migrate/20220310042653_create_employees.rb
@@ -0,0 +1,14 @@
+class CreateEmployees < ActiveRecord::Migration[6.1]
+  def change
+    create_table :employees do |t|
+      t.string :name
+      t.integer :age
+      t.string :status
+      t.string :gender
+
+      t.belongs_to :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
