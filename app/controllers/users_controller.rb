9da1d6ce1864c97f54940a31ff2f352bea6ff842
Victor <victorrivera714@yahoo.com>
2022-03-11 10:23:56 -0800

    user create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+
+
+        #sign up 
+        def create
+            user = User.create(user_params)
+            render json: user
+
+        end
+
+
+
+
+
+
+        private
+
+        def user_params
+            params.permit(:username, :password)
+
+        end
 end
