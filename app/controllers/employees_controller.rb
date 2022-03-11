c042a8ee63998aed99433fa46ffca514834ff30d
Victor <victorrivera714@yahoo.com>
2022-03-10 20:24:27 -0800

    employee index, create,show,private

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,36 @@
 class EmployeesController < ApplicationController
+
+    def index
+        employees = Employee.all
+        render json: employees
+    end
+
+    def create
+        employee = Employee.create(employee_params)
+        render json: employee
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+    def show
+        employee = Employee.find(params[:id])
+        render json: employee
+
+    end
+
+        private
+
+        def employee_params
+            params.permit(:name, :age, :status, :gender, :user_id )
+
+        end
+
+
+
 end
