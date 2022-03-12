0d7f3306112a89e38249fe59f25d403ab720a978
Victor <victorrivera714@yahoo.com>
2022-03-11 17:34:21 -0800

    Employee function

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
+import Employees from './Employees';
 
 function App() {
 
-  
+
 
 
   return (
     <div className="App">
       <header className="App-header">
        
+        <Employees/> 
         
       </header>
     </div>
---
+++ client/src/Employees.js
@@ -0,0 +1,12 @@
+import { render } from "react-dom"
+
+function Employees() {
+    return (
+        <h1> Employees </h1>
+
+
+    )
+
+}
+
+export default Employees
