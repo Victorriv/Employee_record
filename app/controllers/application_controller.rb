5d1e6463f5df7ee4cf415b288be5ec3d8679af43
Victor <victorrivera714@yahoo.com>
2022-03-11 17:56:44 -0800

    authorized

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
-  
+  before_action :authorize
 
+  private 
+
+ def authorize
+  @current_user = User.find_by_id(session[:user_id])
+  render json: {errors: "Not authorized"}, status: :unauthorized unless @current_user
+ end
+  
   
 end
