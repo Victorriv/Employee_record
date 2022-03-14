60cebd179216ba9c474cf87fb7d8d29fe8969ba7
Victor <victorrivera714@yahoo.com>
2022-03-14 12:57:22 -0700

    useEffect to let user see own profile only

--- client/src/Employees.js
+++ client/src/Employees.js
@@ -4,6 +4,14 @@ import React, {useEffect, useState} from "react"
 function Employees() {
     const[employees, setEmployees] = useState([])
 
+    useEffect(()=> {
+        fetch(`/users/${user.id}`)
+        .then((r) => r.json())
+        .then((d) => {
+            setDogs(d.employees)
+        })
+    }, [])  
+
 
 
     return (
