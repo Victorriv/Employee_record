bd0a90651d8032732a5c2ee9929cf00efb4bf97c
Victor <victorrivera714@yahoo.com>
2022-03-15 10:35:02 -0700

    onSubmit for employee form

--- client/src/EmployeeForm.js
+++ client/src/EmployeeForm.js
@@ -10,7 +10,23 @@ function EmployeeForm(){
 
     return(
         <div>
+            <form onSubmit={handleSubmit}>
+                <h2>Add Employee</h2>
 
+                <label> Name: </label>
+                <input type= "text" id="name" value={name} onChange={e => setName(e.target.value)} />
+
+                <label> Age: </label>
+                <input type= "text" id="age" value={age} onChange={e => setAge(e.target.value)}/>
+
+                <label>Status: </label>
+                <input type= "text" id="status" value={status} onChange={e => setStatus(e.target.value)}/>
+
+                <label>Gender</label>
+                <input type="text" id="gender" value={gender} onChange={e => setGender(e.target.value)}/>
+                
+                
+            </form>
 
 
         </div>
