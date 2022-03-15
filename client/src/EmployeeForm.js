d066fb3e844bb8e448e541ab6d330cf87e452694
Victor <victorrivera714@yahoo.com>
2022-03-15 10:14:42 -0700

    UseState for employee form

--- client/src/EmployeeForm.js
+++ client/src/EmployeeForm.js
@@ -2,7 +2,11 @@ import {useState} from "react"
 
 
 function EmployeeForm(){
-    
+    const [name, setName] = useState("")
+    const [age, setAge] = useState("")
+    const [status, setStatus] = useState("")
+    const [gender, setGender] = useState("")
+
 
     return(
         <div>
