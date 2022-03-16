2623e5547e9298647d5823d34cf77886776109ca
Victor <victorrivera714@yahoo.com>
2022-03-16 15:21:08 -0700

    About function

---
+++ client/src/About.js
@@ -0,0 +1,10 @@
+function About(){
+    return (
+        <>
+            <h2>This is app that will help you keep track of all your employees</h2>
+            <p>This lets you add or remove employees, it also allows you to put thier status</p>
+        </>
+    )
+}
+
+export default About
