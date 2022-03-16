39e603e416b5313b4af73f2344cccfb26747be3e
Victor <victorrivera714@yahoo.com>
2022-03-15 20:32:31 -0700

    handleSubmit/fetch

--- client/src/SignUpForm.js
+++ client/src/SignUpForm.js
@@ -3,6 +3,27 @@ import React, {useState} from "react"
 
 function SignUpForm(){
     const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+
+    function handleSubmit(e){
+        e.preventDefault() 
+        fetch("/users", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            }, 
+            body: JSON.stringify({
+                username,
+                password,
+            }),
+        }).then(r => {
+            console.log(r)
+             r.json().then(user => setUser(user));
+
+         });
+    }
+
 
 
 
